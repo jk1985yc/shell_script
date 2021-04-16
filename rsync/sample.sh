d18f318fa483aeb72d04e29f4aa19ce4cfed609d
Deploy_Server <you@example.com>
2021-04-16 12:10:41 +0800

    add rsync sample

---
+++ rsync/sample.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DATE=`date '+%Y%m%d-%H:%M'`
+
+PROJECT=
+SRC=
+DEST=
+
+#Update SRC
+git -C $SRC/$PROJECT pull
+
+#Update DEST
+git -C $DEST/$PROJECT pull
+
+#Merge to DEST
+rsync -avh --delete --exclude-from='./exclude.env' $SRC/$PROJECT/ $DEST/$PROJECT
+git -C $DEST/$PROJECT add --all
+git -C $DEST/$PROJECT commit -m "Update_$DATE"
+git -C $DEST/$PROJECT push
